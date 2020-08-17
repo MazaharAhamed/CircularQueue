ceb8600b9aebe847b7e69ef2c73d273ee1b604b1
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-17 19:42:15 +0530

    Add project files.

---
+++ CircularQueue/CircularQ.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+
+namespace CircularQueue
+{
+    class CircularQ
+    {
+        private int[] CircQA;
+        private int front;
+        private int rear;
+
+        public CircularQ()
+        {
+            CircQA = new int[10];
+            front = -1;
+            rear = -1;
+        }
+
+        public CircularQ(int maxSize)
+        {
+            CircQA = new int[maxSize];
+            front = -1;
+            rear = -1;
+        }
+        public bool isEmpty()
+        {
+            return (front == -1);
+        }
+
+        public bool isFull()
+        {
+            return ((front == 0 && rear == CircQA.Length - 1) || (front == rear + 1));
+        }
+
+        public int Insert(int x)
+        {
+            if (isFull())
+                throw new System.InvalidOperationException("Queue Overflow");
+            if (front == -1)
+                front = 0;
+            if (rear == CircQA.Length - 1)
+                rear = 0;
+            else
+                rear = rear + 1;
+            CircQA[rear] = x;
+            return x;
+        }
+
+        public int Delete()
+        {
+            if (isEmpty())
+                throw new System.InvalidOperationException("Queue Underflow");
+            int x = CircQA[front];
+
+            if(front == rear)            /*Queue has one element*/
+            {
+                front = -1;
+                rear = -1;
+            }
+            if (front == CircQA.Length - 1)
+                front = 0;
+            else
+                front = front + 1;
+            return x;
+        }
+
+        public int Peek()
+        {
+            if (isEmpty())
+                throw new System.InvalidOperationException("Queue Underflow");
+            return CircQA[front];
+        }
+
+        public void Display()
+        {
+            if (front == -1)
+                Console.WriteLine("Queue is Empty");
+            int i = front;
+            if(front<=rear)
+            {
+                while (i <= rear)
+                    Console.WriteLine(CircQA[i++] +" ");
+            }
+            else
+            {
+                while (i <= CircQA.Length - 1)
+                    Console.WriteLine(CircQA[i++] + " ");
+                i = 0;
+                while (i <= rear)
+                    Console.WriteLine(CircQA[i++] + " ");
+            }
+            Console.WriteLine("");
+        }
+
+        public int Size()
+        {
+            if (isEmpty())
+                Console.WriteLine("Queue is Empty");
+            if (isFull())
+                Console.WriteLine(CircQA.Length - 1);
+            int i = front;
+            int s = 0; ;
+            if(front<=rear)
+            {
+                while(i<=rear)
+                {
+                    s++;
+                    i++;
+                }
+            }
+            else
+            {
+                while (i <= CircQA.Length - 1)
+                {
+                    s++;
+                    i++;
+                }
+                i = 0;
+                while (i <= rear)
+                {
+                    s++;
+                    i++;
+                }
+            }
+            return s;
+        }
+    }
+}
---
+++ CircularQueue/Demo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircularQueue
+{
+    class Demo
+    {
+        static void Main(String[] args)
+        {
+            int choice, x;
+
+            CircularQ cqA = new CircularQ();
+
+            while(true)
+            {
+                Console.WriteLine("1.Insert Element in Queue");
+                Console.WriteLine("2.Delete Element from Queue");
+                Console.WriteLine("3.Front Element in Queue");
+                Console.WriteLine("4.Display all elements from Queue");
+                Console.WriteLine("5.Queue Size");
+                Console.WriteLine("6.Quit");
+                Console.WriteLine("Enter Your Choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 6)
+                    break;
+
+                switch(choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the element to insert:");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        cqA.Insert(x);
+                        break;
+                    case 2:
+                        Console.WriteLine("Deleted Element is: " + cqA.Delete());
+                        break;
+                    case 3:
+                        Console.WriteLine("Front Element in Queue is: " + cqA.Peek());
+                        break;
+                    case 4:
+                        cqA.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("Queue Size is: " + cqA.Size());
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice:");
+                        break;
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
